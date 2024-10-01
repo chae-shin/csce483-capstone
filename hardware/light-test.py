174d7e1a90390f9d3355f89c56c8d89240b619a8
Rijul Kakar <rijulkakar1@gmail.com>
2024-10-01 17:31:58 +0100

    Added the completion function

--- hardware/light-test.py
+++ hardware/light-test.py
@@ -8,9 +8,10 @@
 import time
 from rpi_ws281x import *
 import argparse
+import random
 
 # LED strip configuration:
-LED_COUNT      = 300      # Number of LED pixels
+LED_COUNT      = 60      # Number of LED pixels
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses Pulse Width Modulation)
 # If we want to use SPI, we can use the pin 10
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -33,12 +34,28 @@ def turn_off(strip):
 
 # Function to turn LEDs on one by one (previous one turns off)
 def turn_on_one_by_one(strip):
-    for i in range(strip.numPixels() - 1, -1, -1):
-        if i < strip.numPixels() - 1:
-            strip.setPixelColor(i+1, Color(0, 0, 0)) # Off
-        strip.setPixelColor(i, Color(0, 255, 255)) # Cyan
+    for i in range(strip.numPixels() - 1, -7, -1):
+        strip.setPixelColor(i+5, Color(0, 0, 0)) # Off
+        strip.setPixelColor(i, Color(106, 13, 173)) #Purple
         strip.show()
-        time.sleep(0.1)
+        time.sleep(0.05)
+
+# def turn_on_one_by_one(strip):
+#     for i in range(strip.numPixels()):
+#         if i > 0:
+#             strip.setPixelColor(i-1, Color(0,0,0))
+#         strip.setPixelColor(i, Color(0, 255, 255))
+#         strip.show()
+#         time.sleep(0.05)
+
+# Completion Script - Turns on all LEDs with random colors
+def completion(strip):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))) # Random Colors
+        strip.show()
+        time.sleep(0.01)
+    
+
 
 # Main function
 if __name__ == '__main__':
@@ -63,13 +80,20 @@ if __name__ == '__main__':
             time.sleep(1)
             print ('Turning off...')
             turn_off(strip)
-            time.sleep(1)
+            # time.sleep(1)
             print ('Turning on one by one...')
             turn_on_one_by_one(strip)
             time.sleep(1)
             print ('Turning off...')
             turn_off(strip)
             time.sleep(1)
+            turn_on_one_by_one(strip)
+            # print('Completion...')
+            # completion(strip)
+            # time.sleep(2)
+            # print('Turning off...')
+            # turn_off(strip)
+            # time.sleep(1)
 
     except KeyboardInterrupt:
         if args.clear:
