fac7ce92dad123451b3a6518dd4f324df3d1a112
Rijul Kakar <rijulkakar1>
2024-10-29 13:32:57 -0500

    4 notes working

--- hardware/led/multiple_notes_to_led.py
+++ hardware/led/multiple_notes_to_led.py
@@ -1,8 +1,3 @@
-#####################################
-# Working with 2 sepeate midi files #
-#####################################
-
-
 import numpy as np
 from mido import MidiFile
 import time
@@ -10,7 +5,7 @@ from rpi_ws281x import PixelStrip, Color
 import threading
 
 # Constants for LED strip
-LED_COUNT      = 100     # Number of LED pixels.
+LED_COUNT      = 200     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
@@ -53,33 +48,63 @@ def midi_to_led_array(midi_file_path, note, color):
 
     return led_array
 
-# Function to update the left half of the LED strip
-def update_left_half(strip, led_array):
-    led_strip = [(0, 0, 0)] * (LED_COUNT // 2)
-    for i in range(len(led_array) + LED_COUNT // 2):
-        for j in range(LED_COUNT // 2 - 1):
+# Function to update the first quarter of the LED strip
+def update_first_quarter(strip, led_array):
+    led_strip = [(0, 0, 0)] * (LED_COUNT // 4)
+    for i in range(len(led_array) + LED_COUNT // 4):
+        for j in range(LED_COUNT // 4 - 1):
             led_strip[j] = led_strip[j + 1]
         if i < len(led_array):
-            led_strip[LED_COUNT // 2 - 1] = led_array[i]
+            led_strip[LED_COUNT // 4 - 1] = led_array[i]
         else:
-            led_strip[LED_COUNT // 2 - 1] = (0, 0, 0)
-        for k in range(LED_COUNT // 2):
+            led_strip[LED_COUNT // 4 - 1] = (0, 0, 0)
+        for k in range(LED_COUNT // 4):
             strip.setPixelColor(k, Color(*led_strip[k]))
         strip.show()
         time.sleep(0.1)
 
-# Function to update the right half of the LED strip
-def update_right_half(strip, led_array):
-    led_strip = [(0, 0, 0)] * (LED_COUNT // 2)
-    for i in range(len(led_array) + LED_COUNT // 2):
-        for j in range(LED_COUNT // 2 - 1, 0, -1):
+# Function to update the second quarter of the LED strip
+def update_second_quarter(strip, led_array):
+    led_strip = [(0, 0, 0)] * (LED_COUNT // 4)
+    for i in range(len(led_array) + LED_COUNT // 4):
+        for j in range(LED_COUNT // 4 - 1, 0, -1):
             led_strip[j] = led_strip[j - 1]
         if i < len(led_array):
             led_strip[0] = led_array[i]
         else:
             led_strip[0] = (0, 0, 0)
-        for k in range(LED_COUNT // 2, LED_COUNT):
+        for k in range(LED_COUNT // 4, LED_COUNT // 2):
+            strip.setPixelColor(k, Color(*led_strip[k - LED_COUNT // 4]))
+        strip.show()
+        time.sleep(0.1)
+
+# Function to update the third quarter of the LED strip
+def update_third_quarter(strip, led_array):
+    led_strip = [(0, 0, 0)] * (LED_COUNT // 4)
+    for i in range(len(led_array) + LED_COUNT // 4):
+        for j in range(LED_COUNT // 4 - 1):
+            led_strip[j] = led_strip[j + 1]
+        if i < len(led_array):
+            led_strip[LED_COUNT // 4 - 1] = led_array[i]
+        else:
+            led_strip[LED_COUNT // 4 - 1] = (0, 0, 0)
+        for k in range(LED_COUNT // 2, 3 * LED_COUNT // 4):
             strip.setPixelColor(k, Color(*led_strip[k - LED_COUNT // 2]))
+        strip.show()
+        time.sleep(0.1)
+
+# Function to update the fourth quarter of the LED strip
+def update_fourth_quarter(strip, led_array):
+    led_strip = [(0, 0, 0)] * (LED_COUNT // 4)
+    for i in range(len(led_array) + LED_COUNT // 4):
+        for j in range(LED_COUNT // 4 - 1, 0, -1):
+            led_strip[j] = led_strip[j - 1]
+        if i < len(led_array):
+            led_strip[0] = led_array[i]
+        else:
+            led_strip[0] = (0, 0, 0)
+        for k in range(3 * LED_COUNT // 4, LED_COUNT):
+            strip.setPixelColor(k, Color(*led_strip[k - 3 * LED_COUNT // 4]))
         strip.show()
         time.sleep(0.1)
 
@@ -89,29 +114,39 @@ def main():
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
 
-    # Convert MIDI file to LED arrays for A4 and B4
+    # Convert MIDI file to LED arrays for A4, B4, C5, and D5
     led_array_a4 = midi_to_led_array("../midi_files/seperated_notes/note_A4.mid", 69, (0, 0, 255))  # A4 in blue
     led_array_b4 = midi_to_led_array("../midi_files/seperated_notes/note_B4.mid", 71, (255, 0, 0))  # B4 in red
+    led_array_c5 = midi_to_led_array("../midi_files/seperated_notes/note_C5.mid", 72, (0, 255, 0))  # C5 in green
+    led_array_d5 = midi_to_led_array("../midi_files/seperated_notes/note_D5.mid", 74, (255, 255, 0))  # D5 in yellow
 
     # Print the LED arrays for debugging
     print("LED Array Length A4:", len(led_array_a4))
     print("LED Array A4:", led_array_a4)
     print("LED Array Length B4:", len(led_array_b4))
     print("LED Array B4:", led_array_b4)
+    print("LED Array Length C5:", len(led_array_c5))
+    print("LED Array C5:", led_array_c5)
+    print("LED Array Length D5:", len(led_array_d5))
+    print("LED Array D5:", led_array_d5)
 
-    # Create threads for updating the left and right halves of the LED strip
-    left_thread = threading.Thread(target=update_left_half, args=(strip, led_array_a4))
-    right_thread = threading.Thread(target=update_right_half, args=(strip, led_array_b4))
+    # Create threads for updating the quarters of the LED strip
+    first_thread = threading.Thread(target=update_first_quarter, args=(strip, led_array_a4))
+    second_thread = threading.Thread(target=update_second_quarter, args=(strip, led_array_b4))
+    third_thread = threading.Thread(target=update_third_quarter, args=(strip, led_array_c5))
+    fourth_thread = threading.Thread(target=update_fourth_quarter, args=(strip, led_array_d5))
 
-    # Start both threads
-    left_thread.start()
-    right_thread.start()
+    # Start all threads
+    first_thread.start()
+    second_thread.start()
+    third_thread.start()
+    fourth_thread.start()
 
-    # Wait for both threads to finish
-    left_thread.join()
-    right_thread.join()
+    # Wait for all threads to finish
+    first_thread.join()
+    second_thread.join()
+    third_thread.join()
+    fourth_thread.join()
 
 if __name__ == "__main__":
     main()
-
-
