81623735da75b6909b896c60c2c051f0d5bf3118
Sarthak Singh <sarthak3002.singh@gmail.com>
2024-11-06 17:34:11 -0600

    library and song name interaction

--- rabi.py
+++ rabi.py
@@ -7,16 +7,23 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     data = song_data()
-    return render_template("index.html", song_data=data)    
-
+    return render_template("index.html", song_data=data)
+    
+        
 @app.route("/play_song", methods=["POST"])
 def play_song():
+    data = request.get_json()
+    song_index = data.get("song_index")
+
     try:
-        # Run the specific Python file
-        subprocess.run(["python3", "./midiFiles/hello_world_HCB.py"], check=True)
-        return jsonify({"status": "success", "message": "Song is now playing!"})
+        # Run the specific Python file based on the song index
+        song_file = f"./midiFiles/{song_index}_MIDI.py"  # Adjust file path as needed
+        print(song_file)
+        subprocess.run(["python3", song_file], check=True)
+        return jsonify({"status": "success", "message": f"Playing song at index {song_index}!"})
     except subprocess.CalledProcessError as e:
         return jsonify({"status": "error", "message": f"Error playing song: {str(e)}"}), 500
+
 
 @app.route("/currentlyplaying")
 def playing():
