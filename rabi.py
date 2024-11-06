febcca9ffc91eeb0a867bcec71d71cd5b2aac9bb
hellooworld0105 <tlsco8912@tamu.edu>
2024-11-06 14:48:14 -0600

    displaying stats UI after song has played

--- rabi.py
+++ rabi.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from songFilePickStats import song_data
+from Comparsion import total_accuracy, generate_by_note_stat
 import subprocess
 
 app = Flask(__name__)
@@ -24,7 +25,14 @@ def playing():
 
 @app.route("/stats")
 def stats():
-    return render_template("stats.html")
+    data = song_data()
+    # Name of MIDI files
+    midi_reference = "Happy_Birthday_Easy_to_play.mid"
+    midi_user = "user.mid"
+    # Calculate total accuracy and accuracy by notes
+    total_acc = total_accuracy(midi_reference, midi_user)
+    acc_by_notes = generate_by_note_stat(midi_reference, midi_user)
+    return render_template("stats.html", song_data=data, total_accuracy=total_acc, accuracy_by_notes=acc_by_notes)
 
 if __name__ == "__main__":
     app.run(debug=True)
