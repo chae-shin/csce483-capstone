e80b4051466736664b86f7f129b27cf61f9c186a
Sarthak Singh <sarthak3002.singh@gmail.com>
2024-11-11 10:06:07 -0600

    picking song works; basic integration of running lights with recording

--- rabi.py
+++ rabi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify
 from songFilePickStats import song_data
 import subprocess
 import os
@@ -9,7 +9,6 @@ UPLOAD_FOLDER = 'songs/'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Ensure the folder exists
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
 @app.route("/")
 def home():
     data = song_data()
@@ -17,25 +16,20 @@ def home():
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
-    # checks if file chosen
     if 'filename' not in request.files:
         return jsonify({"status": "error", "message": "No file part"}), 400
-    
-    # file is chosen file
     file = request.files['filename']
-    
-    # is file name nothing
     if file.filename == '':
         return jsonify({"status": "error", "message": "No selected file"}), 400
-    
-    # if file perfect we add it to the folder
     if file:
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
-        song_data()
+        return jsonify({"status": "success", "message": "File uploaded successfully!"})
 
-        return jsonify({"status": "success", "message": "File uploaded successfully!", "file_path": file_path})
-
+@app.route('/get_songs', methods=['GET'])
+def get_songs():
+    data = song_data()
+    return jsonify(data)
 
 @app.route("/play_song", methods=["POST"])
 def play_song():
@@ -44,10 +38,10 @@ def play_song():
 
     try:
         # Run the specific Python file based on the song index
-        song_file = song_index # Adjust file path as needed
+        song_file = song_index  # Adjust file path as needed
         print(song_file)
         subprocess.run(["python3", "RunningLights.py", song_file], check=True)
-        return jsonify({"status": "success", "message": f"Playing song at index {song_index}!"})
+        return jsonify({"status": "success", "message": f"Playing song {song_index}!"})
     except subprocess.CalledProcessError as e:
         return jsonify({"status": "error", "message": f"Error playing song: {str(e)}"}), 500
 
